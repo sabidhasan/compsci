7870a90e9064002e42e84e0b7296e663cceab65b
Abid Hasan <abidhasan@gmail.com>
2019-05-11 21:04:08 -0600

    Add PS1 pt B for MIT60001

--- MIT60001-ps1.py
+++ MIT60001-ps1.py
@@ -24,3 +24,25 @@ while current_savings < total_cost * PORTION_DOWN_PAYMENT:
   month += 1
 
 print ("It will take you", month, "months to save for the down payment.")
+
+
+#################
+#    ps2a.py    #
+# House Hunting #
+#################
+total_cost = integer_input('What is the cost for the home? ')
+annual_salary = integer_input('What is your starting annual salary? ')
+portion_saved = integer_input('What percentage of salary needs to be saved? ') / 100
+semiannual_raise = integer_input('What percentage is the semi-annual salary raise? ') / 100
+annual_return = 0.04
+current_savings = 0
+
+month = 0
+while current_savings < total_cost * PORTION_DOWN_PAYMENT:
+  current_savings += (annual_salary / 12 * portion_saved) + (current_savings * annual_return / 12)
+  month += 1
+  
+  if ((month + 1) % 6 == 0):
+    annual_salary *= (1 + semiannual_raise)
+
+print ("It will take you", month, "months to save for the down payment.")
