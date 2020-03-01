8d4be435dd89699d1a4cfc888d00314d56656506
Abid Hasan <abidhasan@gmail.com>
2020-02-29 19:04:02 -0700

    Add lab 5

---
+++ CS61A/labs/lab5/lab05_extra.py
@@ -0,0 +1,103 @@
+""" Optional questions for Lab 05 """
+
+import random
+from lab05 import *
+
+# Shakespeare and Dictionaries
+def build_successors_table(tokens):
+    """Return a dictionary: keys are words; values are lists of successors.
+
+    >>> text = ['We', 'came', 'to', 'investigate', ',', 'catch', 'bad', 'guys', 'and', 'to', 'eat', 'pie', '.']
+    >>> table = build_successors_table(text)
+    >>> sorted(table)
+    [',', '.', 'We', 'and', 'bad', 'came', 'catch', 'eat', 'guys', 'investigate', 'pie', 'to']
+    >>> table['to']
+    ['investigate', 'eat']
+    >>> table['pie']
+    ['.']
+    >>> table['.']
+    ['We']
+    """
+    table = {}
+    prev = '.'
+    for word in tokens:
+        if prev not in table:
+            table[prev] = []
+        table[prev].append(word)
+        prev = word
+    return table
+
+def construct_sent(word, table):
+    """Prints a random sentence starting with word, sampling from
+    table.
+
+    >>> table = {'Wow': ['!'], 'Sentences': ['are'], 'are': ['cool'], 'cool': ['.']}
+    >>> construct_sent('Wow', table)
+    'Wow!'
+    >>> construct_sent('Sentences', table)
+    'Sentences are cool.'
+    """
+    import random
+    result = ''
+    while word not in ['.', '!', '?']:
+        result += word + ' '
+        word = random.choice(table[word])
+    return result.strip() + word
+
+def shakespeare_tokens(path='shakespeare.txt', url='http://composingprograms.com/shakespeare.txt'):
+    """Return the words of Shakespeare's plays as a list."""
+    import os
+    from urllib.request import urlopen
+    if os.path.exists(path):
+        return open('shakespeare.txt', encoding='ascii').read().split()
+    else:
+        shakespeare = urlopen(url)
+        return shakespeare.read().decode(encoding='ascii').split()
+
+# Uncomment the following two lines
+# tokens = shakespeare_tokens()
+# table = build_successors_table(tokens)
+
+def random_sent():
+    import random
+    return construct_sent(random.choice(table['.']), table)
+
+# Q6
+def sprout_leaves(t, vals):
+    """Sprout new leaves containing the data in vals at each leaf in
+    the original tree t and return the resulting tree.
+
+    >>> t1 = tree(1, [tree(2), tree(3)])
+    >>> print_tree(t1)
+    1
+      2
+      3
+    >>> new1 = sprout_leaves(t1, [4, 5])
+    >>> print_tree(new1)
+    1
+      2
+        4
+        5
+      3
+        4
+        5
+
+    >>> t2 = tree(1, [tree(2, [tree(3)])])
+    >>> print_tree(t2)
+    1
+      2
+        3
+    >>> new2 = sprout_leaves(t2, [6, 1, 2])
+    >>> print_tree(new2)
+    1
+      2
+        3
+          6
+          1
+          2
+    """
+    if is_leaf(t):
+      br = [tree(v) for v in vals]
+    else:
+      br = [sprout_leaves(b, vals) for b in branches(t)]
+    return tree(label(t), br)
